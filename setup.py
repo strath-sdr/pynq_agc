3807e907fdc6d175bd46477079c5e5fdf666ce9d
josh goldsmith <josh@spectrum.horse>
2022-04-01 10:16:39 +0100

    upgrades for rfsoc-studio integration

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ def copy_overlay():
 # copy notebooks to jupyter home
 def copy_notebooks():
     src_nb_dir = os.path.join(repo_notebook_folder)
-    dst_nb_dir = os.path.join(board_notebooks_dir, package_name)
+    dst_nb_dir = os.path.join(board_notebooks_dir, 'rfosc-studio', package_name)
     if os.path.exists(dst_nb_dir):
         shutil.rmtree(dst_nb_dir)
     copy_tree(src_nb_dir, dst_nb_dir)
@@ -42,7 +42,7 @@ copy_notebooks()
 
 setup(
     name=package_name,
-    version='0.3.2',
+    version='0.3.3',
     install_requires=[
         'pynq>=2.6',
         'plotly>=4.5.2',
